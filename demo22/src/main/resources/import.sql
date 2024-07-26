5b6ba4abc05da587277c97f2e7211202e1281ae7
thiagorabaioli <tfrabaioli@outlook.com>
2024-07-26 16:59:48 +0100

    Algumas alterações nos papeis

--- demo22/src/main/resources/import.sql
+++ demo22/src/main/resources/import.sql
@@ -1,9 +1,14 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex',  'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria',  'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex Alex2',  'alex@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Bob Bob2',  'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria Maria2',  'maria@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 
-INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_VIGILANTE');
+INSERT INTO tb_role (authority) VALUES ('ROLE_RO');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
